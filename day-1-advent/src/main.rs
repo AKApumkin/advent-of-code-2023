d72523dd2cd1cee9e76b298b1e0df7b9c646dbc1
Patrick Williams <paddywillow@hotmail.com>
2023-12-07 15:27:10 +0000

    refactored and added checks for written words

--- day-1-advent/src/main.rs
+++ day-1-advent/src/main.rs
@@ -1,3 +1,4 @@
+use std::i32;
 /**
  * Take in a file with mixed strings
  * Extract out each lines interger pairs
@@ -9,36 +10,41 @@ use std::fs::File;
 use regex::Regex;
 
 fn main() {
-    let file_path = "src/input.txt";
-    let whole_file = filename_to_string(file_path).unwrap();
-    let wbyl = words_by_line(&whole_file);
+    let file_path: &str    = "src/input.txt";
+    let whole_file: String = filename_to_string(file_path).unwrap();
+    let wbyl: Vec<String>  = words_by_line(&whole_file);
 
-    let mut count = 0;
+    let mut count: i32 = 0;
     for line in wbyl {
+        let mut replaced_string: String  = line.to_string();
+        let numbers: [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+        for (index,number) in numbers.iter().enumerate()
+        {
+            let current_number = index + 1;
+            let current_number_string:String = current_number.to_string();
+            replaced_string = replaced_string.replace(number, current_number_string.as_str());
+        }
+        let re = Regex::new(r"[0-9]").unwrap();
+        let the_matches: Vec<_> = re.find_iter(replaced_string.as_str()).map(|mat| mat.as_str()).collect();
+  
+        let first_number: &str = the_matches.clone().into_iter().next().unwrap();
+        let last_number: &str  = the_matches.clone().into_iter().next_back().unwrap();
 
-        let matches = match_regex_numbers(&line).unwrap().get(0).unwrap().as_str();
+        let combined: String  = first_number.to_string() + &last_number.to_string();
 
-        let first_char = matches.chars().next().unwrap();
-        let last_char = matches.chars().next_back().unwrap();
-
-        let combined_number = first_char.to_string() + &last_char.to_string();
-
-        let to_int:i32 = combined_number.parse().unwrap();
-
-        // add each int together then add to the global count
-        count += to_int;
-        println!("{:?}", count);
+        let final_number:i32 = combined.parse::<i32>().unwrap();
+        count += final_number;
+        print!("{} ", count);
     }
     println!("The final count is: {}", count);
 }
-
 
 /**
  * Read a file into a string
  */
 fn filename_to_string(s: &str) -> io::Result<String> {
-    let mut file = File::open(s)?;
-    let mut s = String::new();
+    let mut file: File = File::open(s)?;
+    let mut s: String = String::new();
     file.read_to_string(&mut s)?;
     Ok(s)
 }
@@ -49,16 +55,5 @@ fn filename_to_string(s: &str) -> io::Result<String> {
  * @return a vector of strings
  */
 fn words_by_line<'a>(s: &'a str) -> Vec<String> {
-    s.split_whitespace().map(|s| s.to_string()).collect()
-}
-
-/**
- * Match a regex pattern to a string finding the numbers
- * @param s: a string
- * @return a string of numbers
- */
-fn match_regex_numbers(s: &str) -> Option<regex::Captures<'_>> {
-    let re = Regex::new(r"[0-9]+").unwrap();
-    let matches = re.captures(s);
-    return matches;
+    s.split_whitespace().map(|s: &str| s.to_string()).collect()
 }
